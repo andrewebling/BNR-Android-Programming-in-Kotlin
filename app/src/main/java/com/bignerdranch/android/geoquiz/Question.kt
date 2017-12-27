a8b4d6cef37cdd4f80cdcbd277948c751575c81a
andrewebling <andrew@ebling.me.uk>
2017-12-27 18:58:36 +0000

    Load questions dynamically.

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -0,0 +1,3 @@
+package com.bignerdranch.android.geoquiz
+
+data class Question constructor(val mTextResId: Int, val mAnswerTrue: Boolean)
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.kt
@@ -4,16 +4,30 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class QuizActivity : AppCompatActivity() {
 
-    lateinit var mTrueButton: Button
-    lateinit var mFalseButton: Button
+    private lateinit var mTrueButton: Button
+    private lateinit var mFalseButton: Button
+    private lateinit var mQuestionTextView: TextView
+
+    private val mQuestionBank = arrayOf(
+            Question(R.string.question_australia, true),
+            Question(R.string.question_oceans, true),
+            Question(R.string.question_mideast, false),
+            Question(R.string.question_africa, false),
+            Question(R.string.question_asia, true))
+    private var mCurrentQuestionIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        mQuestionTextView = findViewById<TextView>(R.id.question_text_view)
+        val questionId = mQuestionBank[mCurrentQuestionIndex].mTextResId
+        mQuestionTextView.setText(questionId)
 
         mTrueButton = findViewById<Button>(R.id.true_button)
         mTrueButton.setOnClickListener {
