443ec88b3a6fb71a402191998111ea0936c52cee
andrewebling <andrew@ebling.me.uk>
2017-12-27 15:05:33 +0000

    Chapter 1 challenge complete.

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -18,9 +19,11 @@ class QuizActivity : AppCompatActivity() {
         mTrueButton = findViewById<Button>(R.id.true_button)
         mTrueButton.setOnClickListener {
 
-                Toast.makeText(this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show()
+            val toast = Toast.makeText(this,
+                    R.string.correct_toast,
+                    Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
         }
 
         mFalseButton = findViewById<Button>(R.id.false_button)
